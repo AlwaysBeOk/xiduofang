0282bda30421b1cc545a1302d5a280e53d7d0a6d
pangguitao <guitao.pang@tendcloud.com>
2019-12-02 14:05:35 +0800

    路径优化

---
+++ precache-manifest.5b3caceee3b9e28ba5354e0a300d08bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "572681fc04fa23707d7b",
+    "url": "./static/js/main.403d6298.chunk.js"
+  },
+  {
+    "revision": "2da9d5e890312f2e9ab7",
+    "url": "./static/js/2.0452d9ea.chunk.js"
+  },
+  {
+    "revision": "572681fc04fa23707d7b",
+    "url": "./static/css/main.0b3dd9ce.chunk.css"
+  },
+  {
+    "revision": "2da9d5e890312f2e9ab7",
+    "url": "./static/css/2.a18e3729.chunk.css"
+  },
+  {
+    "revision": "19363465202b5d50065ada8bd738cad1",
+    "url": "./index.html"
+  }
+];
